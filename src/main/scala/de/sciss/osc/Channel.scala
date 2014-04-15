477d687e11c24d8f1fe10140a45f4fa0185a3c2b
Hanns Holger Rutz <contact@sciss.de>
2014-04-15 16:29:14 +0200

    wrong date

--- src/main/scala/de/sciss/osc/Channel.scala
+++ src/main/scala/de/sciss/osc/Channel.scala
@@ -2,7 +2,7 @@
  * Channel.scala
  * (ScalaOSC)
  *
- * Copyright (c) 2018-2014 Hanns Holger Rutz. All rights reserved.
+ * Copyright (c) 2008-2014 Hanns Holger Rutz. All rights reserved.
  *
  * This software is published under the GNU Lesser General Public License v2.1+
  *
